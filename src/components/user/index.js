d4e1fe4d597ee9cb02f2a7b62c954666477f27e9
Grabtot <dneshotkin@gmail.com>
2023-03-03 18:10:42 +0200

    User component

--- src/components/user/index.js
+++ src/components/user/index.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
-
+import './index.css';
 class User extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isVisible: true
+    }
+  }
+
+  switchSmile = () => {
+    console.log("switch");
+    this.setState({ isVisible: !this.state.isVisible })
+  }
+
+
   render() {
-    const {name} = this.props;
+    const { name, avatar, married } = this.props;
+    const smile = married ? "💔" : "💕";
     return (
       <article>
-        <h1>{name}</h1>
-        <img src='https://cdn.pixabay.com/photo/2020/07/14/13/07/icon-5404125_960_720.png' ></img>
+        <h1>{name} {this.state.isVisible && smile}</h1>
+        <img src={avatar}></img>
+        <button onClick={this.switchSmile}>{this.state.isVisible
+          ? "Hide" : "Show"} married</button>
       </article>
     );
   }
+
+
 }
 
 export default User;
